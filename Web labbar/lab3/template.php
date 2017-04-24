c12b6db43e5ceeb3ba9a4835103aae617b889b13
Flarpz <Marcuskarrm@gmail.com>
2017-04-24 16:29:01 +0200

    update-rep

--- Web labbar/lab3/template.php
+++ Web labbar/lab3/template.php
@@ -1,9 +1,33 @@
-<link rel="stylesheet" href="css/stylesheet.css">
+<link rel="stylesheet" property="stylesheet" type="text/css"
+href="css/stylesheet.css">
 <?php
-$navigation = <<<EOT
+session_name('Website');
+session_start();
+$host = "localhost";
+$user = "marcka14";
+$pwd = "Be1VdnELNw";
+$db = "marcka14_db";
+$mysqli = new mysqli($host, $user, $pwd, $db);
+$navigation = <<<END
 <nav>
- <a href="index.php">Home</a>
- <a href="about.php">About</a>
-</nav>
-EOT;
+<a href="index.php">Home</a>
+<a href="about.php">About</a>
+<a href="products.php">Products</a>
+END;
+
+if (isset($_SESSION['userId']))
+{
+$navigation .= <<<END
+<a href="logout.php">Logout</a>
+Logged in as {$_SESSION['username']}
+<a href="add_product.php">Add product</a>
+END;
+}
+else{
+$navigation .= <<<END
+<a href="register.php">Register</a>
+<a href="login.php">Login</a>
+END;
+}
+$navigation .= '</nav>';
 ?>
